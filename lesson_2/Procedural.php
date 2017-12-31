51a794edb8afaba80f3398d68f76744ecd143643
Iurii <brain22@inbox.ru>
2017-12-31 15:04:10 +0300

    небольшие правки

--- lesson_2/Procedural.php
+++ lesson_2/Procedural.php
@@ -10,152 +10,150 @@
  * @param $n
  * @return array
  */
-function sieve($n)
-{
-    $s = [];
-    for ($i = 10; $i <= $n; $i++) {
-        $s[] = $i;
-    }
+    function sieve($n)
+    {
+        $s = [];
+        for ($i = 10; $i <= $n; $i++) {
+            $s[] = $i;
+        }
 
-    foreach ($s as $key => $value) {
-        for ($k = 2; $k * $k <= $n; $k++) {
-            if ($value % $k == 0 && $value != $k) {
-                unset ($s[$key]);
+        foreach ($s as $key => $value) {
+            for ($k = 2; $k * $k <= $n; $k++) {
+                if ($value % $k == 0 && $value != $k) {
+                    unset ($s[$key]);
+                }
             }
         }
+        return $s;
     }
-    return $s;
-}
 
-$s = sieve(53);
-$array_keys = ['a', 'b', 'c'];
-$array_chunk = array_chunk($s, 3);
+    $s = sieve(53);
+    $array_keys = ['a', 'b', 'c'];
+    $array_chunk = array_chunk($s, 3);
 
-foreach ($array_chunk as $key => $value) {
-    $array_combine[] = array_combine($array_keys, $value);
-}
+    foreach ($array_chunk as $key => $value) {
+        $array_combine[] = array_combine($array_keys, $value);
+    }
 
-/**
- * @param $arr
- * @return float|int
- */
-function plotrap($arr)
-{
-    $res = 1 / 2 * ($arr['a'] + $arr['b']) * $arr['c'];
-    return $res;
-}
+    /**
+     * @param $arr
+     * @return float|int
+     */
+    function plotrap($arr)
+    {
+        $res = 1 / 2 * ($arr['a'] + $arr['b']) * $arr['c'];
+        return $res;
+    }
 
-foreach ($array_combine as $key => $value) {
-    $res = plotrap($value);
-    $array_combine[$key]['s'] = $res;
-}
+    foreach ($array_combine as $key => $value) {
+        $res = plotrap($value);
+        $array_combine[$key]['s'] = $res;
+    }
 
 
-/**
- * @param $array
- * @return mixed
- */
+    /**
+     * @param $array
+     * @return mixed
+     */
 
-function AlmostMaxNumber($array)
-{
-    $max = max($array);
-    if ($max['s'] > 1400) {
-        $IndexToDelete = array_search($max, $array);
-        unset($array[$IndexToDelete]);
+    function AlmostMaxNumber($array)
+    {
         $max = max($array);
-        return $max;
+        if ($max['s'] > 1400) {
+            $IndexToDelete = array_search($max, $array);
+            unset($array[$IndexToDelete]);
+            $max = max($array);
+            return $max;
+        }
     }
-}
 
-do {
-    $max = AlmostMaxNumber($array_combine);
-    //echo $max['a'] . ' ' . $max['b'] . ' ' . $max['c'] . '<br>';
-} while (0);
+    do {
+        $max = AlmostMaxNumber($array_combine);
+        //echo $max['a'] . ' ' . $max['b'] . ' ' . $max['c'] . '<br>';
+    } while (0);
 
-echo 'размеры трапеции, у которой максимальная площадь, но не больше 1400';
-var_dump(AlmostMaxNumber($array_combine));
+    echo 'размеры трапеции, у которой максимальная площадь, но не больше 1400';
+    var_dump(AlmostMaxNumber($array_combine));
 
-/**
- * @param $o
- * @param $s
- * @return mixed
- */
-function powww($o, $s)
-{
-    $r = $o;
-    for ($i = 1; $i < $s; $i++) {
-        $r = $r * $o;
+    /**
+     * @param $o
+     * @param $s
+     * @return mixed
+     */
+    function powww($o, $s)
+    {
+        $r = $o;
+        for ($i = 1; $i < $s; $i++) {
+            $r = $r * $o;
+        }
+        return $r;
     }
-    return $r;
-}
 
-/**
- * @param $a
- * @param $b
- * @param $c
- * @return mixed
- */
+    /**
+     * @param $a
+     * @param $b
+     * @param $c
+     * @return mixed
+     */
 
-function minf($a, $b, $c)
-{
-    if ($a <= $b && $a <= $c) {
-        return $a;
-    } else if ($b <= $a && $b <= $c && $b) {
-        return $b;
-    } else if ($c <= $a && $c <= $b) {
-        return $c;
+    function minf($a, $b, $c)
+    {
+        if ($a <= $b && $a <= $c) {
+            return $a;
+        } else if ($b <= $a && $b <= $c && $b) {
+            return $b;
+        } else if ($c <= $a && $c <= $b) {
+            return $c;
+        }
     }
-}
 
-/**
- * @param $a
- * @param $b
- * @param $c
- * @return mixed
- */
+    /**
+     * @param $a
+     * @param $b
+     * @param $c
+     * @return mixed
+     */
 
-function raschet($a, $b, $c)
-{
-    $ab = $a * $b;
-    $first = (powww($ab, $c));
+    function raschet($a, $b, $c)
+    {
+        $ab = $a * $b;
+        $first = (powww($ab, $c));
 
-    $ac = $a / $c;
-    $second = (powww($ac, $b));
-    $res = $second % 3;
+        $ac = $a / $c;
+        $second = (powww($ac, $b));
+        $res = $second % 3;
 
-    $min = minf($a, $b, $c);
-    $final = powww($res, $min);
+        $min = minf($a, $b, $c);
+        $final = powww($res, $min);
 
-    $f = $first + $final;
+        $f = $first + $final;
 
-    return $f;
-}
-
-foreach ($array_combine as $key => $value) {
-    $f = raschet($value['a'], $value['b'], $value['c']);
-    $array_combine[$key]['f'] = $f;
-}
-
-//----------------------
-
-for ($i = 0; $i < count($array_combine); $i++) {
-    if (($array_combine[$i]['s'] % 2) == 1) {
-        $array_combine[$i]['s'] &= $array_combine[$i]['s'];
-
+        return $f;
     }
-}
+
+    foreach ($array_combine as $key => $value) {
+        $f = raschet($value['a'], $value['b'], $value['c']);
+        $array_combine[$key]['f'] = $f;
+    }
+
+    for ($i = 0; $i < count($array_combine); $i++) {
+        if (($array_combine[$i]['s'] % 2) == 1) {
+            $array_combine[$i]['s'] &= $array_combine[$i]['s'];
+
+        }
+    }
 
 
-echo '<table border="1">';
-echo '<tr>';
-echo '<th>a</th>';
-echo '<th>b</th>';
-echo '<th>c</th>';
-echo '<th>площадь трапеции</th>';
-echo '<th>результат формулы F</th>';
-echo '</tr>';
+    echo '<table border="1">';
+    echo '<tr>';
+    echo '<th>a</th>';
+    echo '<th>b</th>';
+    echo '<th>c</th>';
+    echo '<th>площадь трапеции</th>';
+    echo '<th>результат формулы F</th>';
+    echo '</tr>';
 
-for ($i = 0; $i<count($array_combine); $i++) {
+    for ($i = 0; $i < count($array_combine); $i++) {
         echo '<tr>';
         echo '<td>' . $array_combine[$i]['a'] . '</td>';
         echo '<td>' . $array_combine[$i]['b'] . '</td>';
@@ -163,32 +161,32 @@ for ($i = 0; $i<count($array_combine); $i++) {
         echo '<td>' . $array_combine[$i]['s'] . '</td>';
         echo '<td>' . $array_combine[$i]['f'] . '</td>';
         echo '</tr>';
-}
-echo '</table>';
+    }
+    echo '</table>';
 
-echo '<br>';
+    echo '<br>';
 
-echo '<table border="1">';
-echo '<tr>';
-echo '<th>a</th>';
-echo '<th>b</th>';
-echo '<th>c</th>';
-echo '<th>площадь трапеции</th>';
-echo '<th>результат формулы F</th>';
-echo '</tr>';
-
-for ($i = 0; $i<count($array_combine); $i++) {
-       if (($array_combine[$i]['s'] % 2) == 1) {
+    echo '<table border="1">';
     echo '<tr>';
-    echo '<td>' . $array_combine[$i]['a'] . '</td>';
-    echo '<td>' . $array_combine[$i]['b'] . '</td>';
-    echo '<td>' . $array_combine[$i]['c'] . '</td>';
-    echo '<td>' . $array_combine[$i]['s'] . '</td>';
-    echo '<td>' . $array_combine[$i]['f'] . '</td>';
+    echo '<th>a</th>';
+    echo '<th>b</th>';
+    echo '<th>c</th>';
+    echo '<th>площадь трапеции</th>';
+    echo '<th>результат формулы F</th>';
     echo '</tr>';
-     }
-}
-echo '</table>';
+
+    for ($i = 0; $i < count($array_combine); $i++) {
+        if (($array_combine[$i]['s'] % 2) == 1) {
+            echo '<tr>';
+            echo '<td>' . $array_combine[$i]['a'] . '</td>';
+            echo '<td>' . $array_combine[$i]['b'] . '</td>';
+            echo '<td>' . $array_combine[$i]['c'] . '</td>';
+            echo '<td>' . $array_combine[$i]['s'] . '</td>';
+            echo '<td>' . $array_combine[$i]['f'] . '</td>';
+            echo '</tr>';
+        }
+    }
+    echo '</table>';
 
 
 
