019bd7f6c11db58982168d2e7ce06ce025b1e739
Iurii <brain22@inbox.ru>
2018-01-04 20:44:01 +0300

    небольшие правки

---
+++ lesson_3/functions.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Вован
+ * Date: 02.01.2018
+ * Time: 0:11
+ */
+
+$str = "abcdbcebcab";
+$substr = "ab";
+
+function revdsstr($str, $substr)
+{
+    $del = "~($substr)~";
+    $arr = preg_split($del, $str, -1, PREG_SPLIT_DELIM_CAPTURE);
+    $del = $substr;
+    $keys = array_keys($arr, $del);
+
+    if (isset($keys[1])) {
+        $revstr = $arr[$keys[1]];
+        $revstr = strrev($revstr);
+        $arr[$keys[1]] = $revstr;
+    } else {
+        return false;
+    }
+
+    $res = implode($arr);
+    return $res;
+}
+
+
+echo revdsstr($str, $substr);
